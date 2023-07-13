69af83f1d41447426472f57e9ad3716fa2ab0e98
Hikali-47041 <hikali.47041@gmail.com>
2023-07-13 10:31:06 +0900

    make str2bool myself

--- syno_nas_upload.py
+++ syno_nas_upload.py
@@ -1,22 +1,29 @@
 import os
 import argparse
-from distutils.util import strtobool
 from pathlib import Path
-
 from dotenv import load_dotenv
 from synology_api import filestation
+
+def str2bool(value):
+    """Convert a string representation of truth to true (1) or false (0).
+    True values are 'true', and '1';
+    False values are anything else.
+    """
+    return value.lower() in ("true", "1")
 
 load_dotenv()
 ip_addr = os.getenv('NAS_IP_ADDR')
 port = os.getenv('NAS_IP_PORT')
 username = os.getenv('NAS_USER_NAME')
 password = os.getenv('NAS_USER_PASSWORD')
-secure = bool(strtobool(os.getenv('SECURE', default="True")))
-cert_verify = bool(strtobool(os.getenv('CERT_VERIFY', default="True")))
+secure = bool(str2bool(os.getenv('SECURE', default="True")))
+cert_verify = bool(str2bool(os.getenv('CERT_VERIFY', default="True")))
 dsm_version = os.getenv('DSM_VERSION')
 otp_code = os.getenv('OTP_CODE')
 
 def nas_upload_main():
+    """ nas upload main method """
+
     # parse command arguments
     parser = argparse.ArgumentParser(description='Synology nas file uploader')
     parser.add_argument('srcpath', help='upload source path', nargs="*")
@@ -36,7 +43,7 @@ def nas_upload_main():
         dir_list += [Path(file).relative_to(Path(srcpath).parent) for file in Path(srcpath).rglob("*") if Path.is_dir(file)]
         srcpath_list += [(Path(file), Path(file).relative_to(Path(srcpath).parent).parent) for file in Path(srcpath).rglob("*") if Path.is_file(file)]
 
-    # connect 
+    # connect
     if verbose_flag:
         print(f"Connecting to {ip_addr}:{port} as {username}")
     fl = filestation.FileStation(ip_addr, port, username, password, secure=secure, cert_verify=cert_verify, dsm_version=dsm_version, debug=verbose_flag, otp_code=otp_code)
